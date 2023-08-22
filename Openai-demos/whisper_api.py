846767b467737c037a2c0f32813977d1da46e0ac
hastur66 <chamathkaubandara@gmail.com>
2023-08-22 20:41:23 +0530

    add subtitle, translation & prompt functionality to whisper demo

--- Openai-demos/whisper_api.py
+++ Openai-demos/whisper_api.py
@@ -14,6 +14,8 @@ filename = 'data/2830-3980-0043.wav'
 # print(transcript)
 # audio_file.close()
 
+
+### Transcribe
 def speech_to_text(filename):
     """
     Transcribe audio file into text
@@ -35,6 +37,73 @@ def speech_to_text(filename):
         transcript = "Error"
 
         return transcript
-        
+
+
+#### Subtitles        
+def speech_to_sub(filename):
+    """
+    Transcribe audio file into subtitles
+    """
+    try:
+        with open(filename, 'rb') as audio_file:
+            subtitles = openai.Audio.transcribe(
+            file = audio_file,
+            model = "whisper-1",
+            response_format="srt",
+            language="en"
+        )
+        return subtitles
+    except Exception as e:
+        print(e)
+        subtitles = "Error"
+
+        return subtitles
+
+
+### Prompt modification
+def speech_prompt_modification(filename):
+    """
+    Optimize outputs with prompts
+    """
+    try:
+        with open(filename, 'rb') as audio_file:
+            prompt_modification = openai.Audio.transcribe(
+            file = audio_file,
+            model = "whisper-1",
+            response_format="text",
+            language="en",
+            prompt="In surprice"
+        )
+        return prompt_modification
+    except Exception as e:
+        print(e)
+        prompt_modification = "Error"
+
+        return prompt_modification
+
+
+### Translation
+def speech_translation(filename):
+    """
+    Translate spanish audio file into english text
+    """
+    try:
+        with open(filename, 'rb') as audio_file:
+            translation = openai.Audio.translate(
+            file = audio_file,
+            model = "whisper-1",
+            response_format="text",
+            language="en"
+            )
+        return translation
+    except Exception as e:
+        print(e)
+        translation = "Error"
+
+        return translation
 
 print(speech_to_text(filename))
+
+print(speech_to_sub(filename))
+
+print(speech_prompt_modification(filename))
