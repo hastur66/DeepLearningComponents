51de595d5302454efe6a6af82d665706ad3c0143
hastur66 <chamathkaubandara@gmail.com>
2023-10-14 01:38:08 +0530

    update kerasNLP gpt2 package imports

--- GPT-2 finetune/kerasNLP-gpt2/load_pretrained.py
+++ GPT-2 finetune/kerasNLP-gpt2/load_pretrained.py
@@ -1,9 +1,9 @@
-import numpy as np
 import keras_nlp
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_text as tf_text
 from tensorflow import keras
+import numpy as np
 import time
 
 
