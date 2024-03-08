3bf5586d5d2bd7940d18f108558235fa37bd2b23
hastur66 <chamathkaubandara@gmail.com>
2024-03-09 00:25:22 +0530

    add anthropic text completion example

---
+++ LLM-demos/Anthropic/text_completion.py
@@ -0,0 +1,20 @@
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+anthropic = Anthropic(
+	api_key=API_KEY,
+	)
+
+completion = anthropic.completions.create(
+	model="claude-2.1",
+	max_tokens_to_sample=350,
+	prompt=f"{HUMAN_PROMPT} How do I learn Python in a week?{AI_PROMPT}",
+	)
+	
+print(f"Response: {completion}")
+print(f"Completion: {completion.completion}")
