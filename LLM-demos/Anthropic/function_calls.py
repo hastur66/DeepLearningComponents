85b390146e4e52484a04a368e11ab62cb09dd3b9
hastur66 <chamathkaubandara@gmail.com>
2024-05-11 00:07:52 +0530

    add anthropic function call example

---
+++ LLM-demos/Anthropic/function_calls.py
@@ -0,0 +1,40 @@
+from anthropic import Anthropic
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+client = Anthropic(
+	api_key=API_KEY,
+	)
+
+response = client.beta.tools.messages.create(
+    model="claude-3-opus-20240229",
+    max_tokens=1024,
+    tools=[
+        {
+            "name": "get_weather",
+            "description": "Get the current weather in a given location",
+            "input_schema": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA"
+                    },
+                    "unit": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"],
+                        "description": "The unit of temperature, either \"celsius\" or \"fahrenheit\""
+                    }
+                },
+                "required": ["location"]
+            }
+        }
+    ],
+    messages=[{"role": "user", "content": "What is the weather like in San Francisco?"}]
+)
+
+print(response)
