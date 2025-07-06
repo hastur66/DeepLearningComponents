d2f089d4dfaab8f5677bbf6e9652dd16f6bf9ab9
hastur66 <chamathkaubandara@gmail.com>
2025-07-06 20:22:11 +0530

    add pydanticai example

---
+++ Agentic-frameworks/PydanticAI/pydanticai_demo.py
@@ -0,0 +1,41 @@
+from pydantic_ai import Agent, RunContext
+from pydantic_ai.models.gemini import GeminiModel
+from pydantic_ai.providers.google_gla import GoogleGLAProvider
+from dotenv import load_dotenv
+import random
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
+
+
+model = GeminiModel(
+    'gemini-2.0-flash', provider=GoogleGLAProvider(api_key=GEMINI_API_KEY)
+)
+
+agent = Agent(
+    model=model,  
+    deps_type=str,  
+    system_prompt=(
+        "You're a dice game, you should roll the die and see if the number "
+        "you get back matches the user's guess. If so, tell them they're a winner. "
+        "Use the player's name in the response."
+    ),
+)
+
+
+@agent.tool_plain  
+def roll_dice() -> str:
+    """Roll a six-sided die and return the result."""
+    return str(random.randint(1, 6))
+
+
+@agent.tool  
+def get_player_name(ctx: RunContext[str]) -> str:
+    """Get the player's name."""
+    return ctx.deps
+
+
+dice_result = agent.run_sync('My guess is 4', deps='Anne')  
+print(dice_result.output)
